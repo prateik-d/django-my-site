cf782691ca7c781a2ac8391eb73363698a01bec0
prateik-d <pratik.dhotmal@gmail.com>
2021-07-12 18:47:00 +0530

    building data & models

--- blog/views.py
+++ blog/views.py
@@ -1,75 +1,35 @@
-from datetime import date
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-all_posts = [
-    {
-        "slug" : "hike-in-the-mountain",
-        "image" : "mountains.jpg",
-        "author" : "Prateik",
-        "date" : date(2021, 7, 9),
-        "title" : "Mountain Hiking",
-        "excerpt" : "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
-        "content" : """
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                       
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                       
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                    """
-
-    },
-    {
-        "slug" : "programming-is-fun",
-        "image" : "coding.jpg",
-        "author" : "Prateik",
-        "date" : date(2021, 4, 9),
-        "title" : "Programming Is Great",
-        "excerpt" : "Programming is the process of creating a set of instructions that tell a computer how to perform a task. Programming can be done using a variety of computer programming languages.",
-        "content" : """
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                    """
-
-    },
-    {
-        "slug" : "into-the-woods",
-        "image" : "woods.jpg",
-        "author" : "Prateik",
-        "date" : date(2021, 5, 8),
-        "title" : "Nature At its Best",
-        "excerpt" : "The beauty of nature The nature of man is characterized by its beauty resulting mainly from the wonderful diversity of living organisms that exist in various parts of the earth, as well as the unique terrain of mountains, water, plateaus and forests.",
-        "content" : """
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                    """
-
-    }
-]
-
-def get_date(post):
-  return post['date']
 
 # Create your views here.
 
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+
+    latest_posts = Post.objects.all().order_by("-date")[:3]
+
+    # sorted_posts = sorted(all_posts, key=get_date)
+    # latest_posts = sorted_posts[-3:]
+
     return render(request, "blog/index.html", {
       "posts": latest_posts
     })
 
 def posts(request):
+    all_posts = Post.objects.all().order_by("date")
     return render(request, "blog/all-post.html", {
         "all_posts" : all_posts
     })
 
 def post_details(request, slug):
     
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    # identified_post = Post.objects.get(slug=slug)
+    identified_post = get_object_or_404(Post, slug=slug)
+
+    # identified_post = next(post for post in all_posts if post['slug'] == slug)
     
     return render(request, 'blog/post-detail.html', {
-        "post" : identified_post
+        "post" : identified_post,
+        "tags" : identified_post.tags.all()
     })
